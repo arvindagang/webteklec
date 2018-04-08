08d01d7cfc87c73c1fe5ee4e2f64416818a75d51
2167488 <35653923+Xenomorphos77@users.noreply.github.com>
2018-04-08 23:31:16 +0800

    asd

---
+++ webtechmid/scripts/action.js
@@ -0,0 +1,154 @@
+function showColor(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "80%";
+	document.getElementById("color").style.height = "5000px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+}
+
+function showFun(){
+	document.getElementById("funda").style.width = "80%";
+	document.getElementById("funda").style.height = "4000px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+}
+
+function showDec(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "80%";
+	document.getElementById("decla").style.height = "3800px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+	
+}
+
+function showSnA(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+}
+
+function dimcopart1(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "80%";
+	document.getElementById("dimcopart1").style.height = "4800px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+	
+}
+
+function dimcopart2(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "80%";
+	document.getElementById("dimcopart2").style.height = "5000px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+	
+}
+
+function dimcopart3(){
+	document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "80%";
+	document.getElementById("dimcopart3").style.height = "5000px";
+	document.getElementById("Prepr").style.width = "0%";
+	document.getElementById("Prepr").style.height = "0px";
+	
+}
+function showPrp(){
+document.getElementById("funda").style.width = "0%";
+	document.getElementById("funda").style.height = "0px";
+	document.getElementById("home").style.width = "0%";
+	document.getElementById("home").style.height = "0px";
+	document.getElementById("decla").style.width = "0%";
+	document.getElementById("decla").style.height = "0px";
+	document.getElementById("color").style.width = "0%";
+	document.getElementById("color").style.height = "0px";
+	document.getElementById("dimcopart1").style.width = "0%";
+	document.getElementById("dimcopart1").style.height = "0px";
+	document.getElementById("dimcopart2").style.width = "0%";
+	document.getElementById("dimcopart2").style.height = "0px";
+	document.getElementById("dimcopart3").style.width = "0%";
+	document.getElementById("dimcopart3").style.height = "0px";
+	document.getElementById("Prepr").style.width = "80%";
+	document.getElementById("Prepr").style.height = "2800px";
+}
